0c97e48d8767689da24c687e8f8ea19ab826d672
Nguyễn Minh Nhựt <nhut.nguyen.dev@outlook.com.vn>
2026-01-18 15:44:18 +0700

    fix(config): apply ignition plugin

--- contracts/hardhat.config.ts
+++ contracts/hardhat.config.ts
@@ -1,6 +1,8 @@
+import hardhatIgnitionPlugin from '@nomicfoundation/hardhat-ignition'
 import { defineConfig } from 'hardhat/config'
 
 export default defineConfig({
+  plugins: [hardhatIgnitionPlugin],
   solidity: '0.8.28',
   networks: {
     hardhatMainnet: {
